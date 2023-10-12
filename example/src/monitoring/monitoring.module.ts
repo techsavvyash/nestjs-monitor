c5674fbcfac52cec0dad535ed11500f504651e31
Tushar <codingid6@gmail.com>
2023-10-12 08:39:33 +0000

    use atomic increments

--- example/src/monitoring/monitoring.module.ts
+++ example/src/monitoring/monitoring.module.ts
@@ -1,7 +1,6 @@
 import { Module, OnModuleInit, Global, DynamicModule } from '@nestjs/common';
 import { MonitoringService } from './monitoring.service';
 import { CacheModule } from '@nestjs/cache-manager';
-import type { RedisClientOptions } from 'redis';
 import * as redisStore from 'cache-manager-redis-store';
 
 @Global()
