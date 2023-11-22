3135aa3d2a9e6b17ebb7408b5bb3c2103b4b8456
soorajk1 <ksooraj162@gmail.com>
2023-11-22 20:56:51 +0530

    setup for publishing

---
+++ example/src/index.ts
@@ -0,0 +1,3 @@
+export * from './monitoring/monitoring.service';
+export * from './interceptors/response-time.interceptor';
+export { MonitoringServiceInterface } from './monitoring/monitoring.service';
